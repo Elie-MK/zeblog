4893f9a97566842d21818a3e57f67883bb5e5ba2
Elie-MK <elie.mk25@gmail.com>
2024-06-30 23:59:27 +0100

    refactor clean

--- utilities/ApiRequestsService.js
+++ utilities/ApiRequestsService.js
@@ -1,33 +1,76 @@
 import AsyncStorage from "@react-native-async-storage/async-storage";
 import axios from "axios";
 
-const API_BASE_URL = "http://192.168.1.114:3000";
+export const API_BASE_URL = "http://192.168.1.114:3000/api";
+export const source = axios.CancelToken.source()
+const tokens = handleGetJwtTokenAsyncStorage();
 
-function handleSaveJwtTokenAsyncStorage(token) {
+const option =   {
+  cancelToken :source.token,
+  timeout:10000, 
+  headers: {
+    "Authorization": `Bearer ${tokens.token}`,
+  },
+}
+
+// LocalStorage
+
+export async function handleSaveJwtTokenAsyncStorage(token) {
   try {
-    AsyncStorage.setItem("jwtToken", JSON.stringify(token));
+    await AsyncStorage.setItem("jwtToken", JSON.stringify(token));
   } catch (error) {
     console.log(error);
   }
 }
 
+export async function handleGetJwtTokenAsyncStorage() {
+  try {
+    const token = await AsyncStorage.getItem("jwtToken");
+    return token ? JSON.parse(token) : null;
+  } catch (error) {
+    console.log(error);
+    return null;
+  }
+}
+
+// Authentication
+
 export async function loginUser(datas) {
-    try {
-        const response = await axios.post(`${API_BASE_URL}/api/auth/login`, datas);
-        if(response === 201){
-            handleSaveJwtTokenAsyncStorage(response.data);
-        }
-        return response;
-    } catch (error) {
-        throw error;
-    }
+  try {
+    const response = await axios.post(`${API_BASE_URL}/auth/login`, datas);
+    return response;
+  } catch (error) {
+    throw error;
+  }
 }
 
 export async function registerUser(datas) {
-    try {
-        const response = await axios.post(`${API_BASE_URL}/api/auth/register`, datas);
-        return response;
-    } catch (error) {
-        throw error;
-    }
+  try {
+    const response = await axios.post(`${API_BASE_URL}/auth/register`, datas);
+    return response;
+  } catch (error) {
+    throw error;
+  }
+}
+
+// Article
+
+export async function handleCreateArticle(datas) {
+  const tokens = await handleGetJwtTokenAsyncStorage();
+  try {
+    const response = await axios.post(
+      `${API_BASE_URL}/articles/create`,
+      datas,
+      {
+        headers: {
+          "Authorization": `Bearer ${tokens.token}`,
+          "Content-Type": "multipart/form-data",
+          "Accept": "application/json",
+        },
+      }
+    );
+    return response;
+  } catch (error) {
+    throw error;
+  }
 }
