b011aaafbb49bc2c32fc2d26f7131a2b8e239a05
Elie-MK <elie.mk25@gmail.com>
2024-07-14 16:16:00 +0100

    add backend data

--- components/PeopleFollowItem.js
+++ components/PeopleFollowItem.js
@@ -3,32 +3,36 @@ import React from "react";
 import { Button } from "@rneui/base";
 import { colors } from "../utilities/Color";
 
-const PeopleFollowItem = ({ followers,  names, username, items, onPress }) => {
-
-  const isFollow = followers.includes(items.username);
-
+const PeopleFollowItem = ({ datas }) => {
   return (
     <View
       style={{
         flexDirection: "row",
         alignItems: "center",
         justifyContent: "space-between",
-        marginBottom:10
+        marginBottom: 10,
       }}
     >
       <View style={{ flexDirection: "row", alignItems: "center", gap: 20 }}>
         <Image
-          resizeMode="contain"
           style={{ width: 80, height: 80, borderRadius: 50 }}
-          source={require("../assets/images/vectorPeople.jpg")}
+          source={
+            datas.pictureProfile
+              ? { uri: datas.pictureProfaile }
+              : require("../assets/images/vectorPeople.jpg")
+          }
         />
         <View>
-          <Text style={{ fontSize: 18, fontWeight: "bold" }}>{names}</Text>
-          <Text style={{ fontSize: 16, color: "gray", marginTop:5 }}>{username}</Text>
+          <Text style={{ fontSize: 18, fontWeight: "bold" }}>
+            {datas?.fullName}
+          </Text>
+          <Text style={{ fontSize: 16, color: "gray", marginTop: 5 }}>
+            @{datas?.username}
+          </Text>
         </View>
       </View>
-      <TouchableOpacity >
-        {isFollow ? (
+      <TouchableOpacity>
+        {true ? (
           <Button
             titleStyle={{ fontWeight: "600" }}
             buttonStyle={{
@@ -37,7 +41,6 @@ const PeopleFollowItem = ({ followers,  names, username, items, onPress }) => {
               borderRadius: 22,
             }}
             title="Follow"
-            onPress={onPress}
           />
         ) : (
           <Button
@@ -45,12 +48,11 @@ const PeopleFollowItem = ({ followers,  names, username, items, onPress }) => {
               backgroundColor: colors.white,
               padding: 8,
               borderRadius: 22,
-              borderWidth:2, 
-              borderColor:colors.main
+              borderWidth: 2,
+              borderColor: colors.main,
             }}
-            titleStyle={{color:colors.main}}
+            titleStyle={{ color: colors.main }}
             title={"Following"}
-            onPress={onPress}
           />
         )}
       </TouchableOpacity>
