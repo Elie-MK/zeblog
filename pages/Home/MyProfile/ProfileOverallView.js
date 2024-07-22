4fb2b6728d4811dd7508090ea3673778cecf85af
Elie-MK <elie.mk25@gmail.com>
2024-07-22 22:45:59 +0100

    update ui

--- pages/Home/MyProfile/ProfileOverallView.js
+++ pages/Home/MyProfile/ProfileOverallView.js
@@ -1,41 +1,52 @@
-import { View, Text, SafeAreaView, TouchableOpacity } from 'react-native'
-import React from 'react'
+import { View, Text, SafeAreaView, TouchableOpacity } from "react-native";
+import React from "react";
 import { Ionicons } from "@expo/vector-icons";
-import { Octicons } from '@expo/vector-icons';
-import { Image } from 'react-native';
+import { Octicons } from "@expo/vector-icons";
+import { Image } from "react-native";
 import { createMaterialTopTabNavigator } from "@react-navigation/material-top-tabs";
 
-import { colors } from '../../../utilities/Color'
-import ProfileItem from '../../../components/ProfileItem';
-import ArticlesWriter from '../../../screens/writer/ArticlesWriter';
-import AboutWriter from '../../../screens/writer/AboutWriter';
-import { Androids } from '../../../utilities/Platform';
+import { colors } from "../../../utilities/Color";
+import ProfileItem from "../../../components/ProfileItem";
+import ArticlesWriter from "../../../screens/writer/ArticlesWriter";
+import AboutWriter from "../../../screens/writer/AboutWriter";
+import { Androids } from "../../../utilities/Platform";
+import useGetRequestApi from "../../../hooks/useGetRequestApi";
 
-
-const ProfileOverallView = ({navigation}) => {
+const ProfileOverallView = ({ navigation }) => {
   const Top = createMaterialTopTabNavigator();
-
+  const currentuser = "profile";
+  const { datas, error, fetchDatas } = useGetRequestApi(currentuser);
   return (
-    <SafeAreaView style={{flex:1, marginHorizontal:20}}>
-      <View style={{flexDirection:"row", marginTop:Androids?20:null,  justifyContent:"space-between", alignItems:"flex-end"}}>
+    <SafeAreaView style={{ flex: 1, marginHorizontal: 20 }}>
+      <View
+        style={{
+          flexDirection: "row",
+          marginTop: Androids ? 20 : null,
+          justifyContent: "space-between",
+          alignItems: "flex-end",
+        }}
+      >
         <View>
-            <Image
-              style={{ width: 120, height: 80 }}
-              source={require("../../../assets/images/profile.png")}
-            />
-          </View>
-          <View style={{flexDirection:"row", alignItems:"center", gap:20}}>
+          <Image
+            style={{ width: 120, height: 80 }}
+            source={require("../../../assets/images/profile.png")}
+          />
+        </View>
+        <View style={{ flexDirection: "row", alignItems: "center", gap: 20 }}>
           <TouchableOpacity>
             <Ionicons name="share-outline" size={30} color={colors.black} />
           </TouchableOpacity>
-          <TouchableOpacity onPress={()=>navigation.navigate('settings')}>
-            <Octicons name="gear" size={24} color={colors.black} />            
+          <TouchableOpacity onPress={() => navigation.navigate("settings")}>
+            <Octicons name="gear" size={24} color={colors.black} />
           </TouchableOpacity>
-          </View>
+        </View>
       </View>
-      <ProfileItem onPress={()=>navigation.navigate('editprofile')} names={"Doe Kanyinda"} username={'@elie_mk'}/>
+      <ProfileItem
+        onPress={() => navigation.navigate("editprofile")}
+        datas={datas}
+      />
       <Top.Navigator
-      sceneContainerStyle={{backgroundColor:colors.white}}
+        sceneContainerStyle={{ backgroundColor: colors.white }}
         screenOptions={({ route, navigation }) => ({
           tabBarLabel: ({ focused }) => {
             let label;
@@ -57,15 +68,13 @@ const ProfileOverallView = ({navigation}) => {
             );
           },
           tabBarIndicatorStyle: { backgroundColor: colors.main, height: 4 },
-          
         })}
       >
         <Top.Screen name="article" component={ArticlesWriter} />
         <Top.Screen name="about" component={AboutWriter} />
       </Top.Navigator>
     </SafeAreaView>
+  );
+};
 
-  )
-}
-
-export default ProfileOverallView
+export default ProfileOverallView;
